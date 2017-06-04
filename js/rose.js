5d970c41eb3b99ca029a234d4e994f808b73ddb4
Cameron Burkholder <Cameron Burkholder>
2017-06-04 12:51:59 -0400

    Initial Commit

---
+++ js/rose.js
@@ -0,0 +1,31 @@
+$("document").ready(function() {
+
+  /* SET COVER IMAGES */
+  $(".cover-image").each(function() {
+    $(this).css("background", "url(" + $(this).data("src") + ")");
+  });
+
+  /* CHANGE NAVBAR ON SCROLL */
+  $("nav").addClass("navbar-initial");
+
+  /* REVEAL .reveal ON SCROLL */
+  $(".reveal").each(function() {
+    $(this).fadeOut();
+  });
+  var half = ($(window).height() / 2);
+  $(window).scroll(function() {
+    $(".reveal").each(function() {
+      if (Math.round($(window).scrollTop() + $(window).height()) > ($(this).offset().top - half)) {
+        $(this).delay(700).fadeIn(700);
+      }
+    });
+
+    /* NAVBAR CHANGE ON SCROLL */
+    if (Math.round($(window).scrollTop()) > $("#top").offset().top) {
+      $("nav").removeClass("navbar-initial");
+    } else {
+      $("nav").addClass("navbar-initial");
+    }
+  });
+
+});
